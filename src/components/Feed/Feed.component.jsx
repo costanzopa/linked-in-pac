51f409df0e8bae1a919124009c250c3488e07037
Pablo Costanzo <pcostanzo@southend.com.ar>
2021-04-26 16:25:35 -0300

    Feed Section/ Input Options

--- src/components/Feed/Feed.component.jsx
+++ src/components/Feed/Feed.component.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import CreateIcon from '@material-ui/icons/Create';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+
+import InputOption from './InputOption';
 
 import './Feed.styles.css';
 
@@ -13,6 +19,16 @@ const Feed = (props) => {
             <input type="text" />
             <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#7FC15E"
+          />
         </div>
       </div>
     </div>
