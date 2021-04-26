dc9266a77bdb096b979e7e75f2cc991cc1086ac6
Pablo Costanzo <pcostanzo@southend.com.ar>
2021-04-26 15:28:43 -0300

    Feed Section with input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
 import './App.css';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </div>
     </div>
